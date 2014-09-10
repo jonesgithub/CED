5d5b9eebcb3c31f3ce03eb08bfeff1bf302906f1
Archer_小A <qddegtya@gmail.com>
2014-09-10 19:11:15 +0800

    调整目录结构

--- CED/CED_homepage/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from CED_homepage.models import *
-
-# Register your models here.
-admin.site.register(ced_issues)
-admin.site.register(ced_keys)
-admin.site.register(ced_types)
-admin.site.register(ced_issue_comments)
-admin.site.register(ced_events)
