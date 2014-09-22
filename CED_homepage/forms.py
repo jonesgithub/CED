6180a53626e876fd28d83c807e608be37c75ee35
Archer_小A <qddegtya@gmail.com>
2014-09-22 17:29:07 +0800

    Revert "删除不必要的资源文件"
    This reverts commit 55eb0818094f0b58a686fb87c9eeb4222ffd44f2.

--- CED_homepage/forms.py
+++ CED_homepage/forms.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+from django import forms
+from ckeditor.widgets import CKEditorWidget
+
+#评论页表单
+class commentForm(forms.Form):
+    #自定义widget,以修改text默认class
+    commentcontent=forms.CharField(widget=CKEditorWidget(),required=True,label=u"")
